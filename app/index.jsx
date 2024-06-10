8dbb6c3758063dc9cad0a78e4d149c58410f2db7
Hiroyuki1313 <munozdiaza@gmail.com>
2024-06-10 13:29:52 -0600

    ss

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pito guarzon</Text>
+      <Text>Pito guarzon 2</Text>
       <StatusBar style="auto" />
     </View>
   );
