ddba76e21689a455e7099609e4100493f715df28
Hiroyuki1313 <munozdiaza@gmail.com>
2024-06-11 08:37:39 -0600

    learning tw

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={tw`self-center bg-red-800`}>
+    <View style={tw`mx-50 bg-red-800`}>
       <Text style={tw``}>Pito guarzon 3 hola y adios</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Pito perfil</Link>
