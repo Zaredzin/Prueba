dd9d464644440ef1ca2d6de3499f888450ce04c8
Hiroyuki1313 <munozdiaza@gmail.com>
2024-06-10 20:47:41 -0600

    prueba twrnc

--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,8 @@ import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-6xl">Pito guarzon 3 hola y adios</Text>
+    <View style={tw`items-center bg-black`}>
+      <Text style={tw``}>Pito guarzon 3 hola y adios</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Pito perfil</Link>
     </View>
