fe8fa2ded516130ad5bd489fc4472d75d7afe535
Hiroyuki1313 <munozdiaza@gmail.com>
2024-06-10 20:36:59 -0600

    twrnc classnames

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
-import {Link} from 'expo-router'
+import {Link} from 'expo-router';
+import tw from 'twrnc'
 
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Pito guarzon 3 hola y adios</Text>
+      <Text className="text-6xl">Pito guarzon 3 hola y adios</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Pito perfil</Link>
     </View>
