abe4412952feb90ab86a94e98d22c396fdf9d908
Zaredzin <JammedMist76@hotmail.com>
2024-06-12 16:49:54 -0600

    Ps si jala

--- app/index.jsx
+++ app/index.jsx
@@ -6,11 +6,24 @@ import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={tw`mx-50 my-6 bg-red-800`}>         
-      <Text style={tw``}>Pito guarzon 3 hola y adios</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{color: 'blue'}}>Pito perfil</Link>
+    <View style={tw ``}>
+      <View style={tw`flex bg-neutral-800 `}>
+        <Text style={tw`text-2xl text-white mt-10 mx-10`}>Pito guarzon 3 hola y adios</Text>
+        <StatusBar style="auto" />
+      </View>     
+      <View style={tw `bg-neutral-300`}>
+        <View>
+          
+        </View>
+        <Link href="/profile" style={tw`flex text-cyan-500 mx-10 `}>Pito perfil</Link>
+        
+      </View>    
+      
+      
+      
     </View>
+    
   );
+  
 }
 
