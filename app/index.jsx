ab1bd2bd23d900e2bae14c48182eb29786513f6c
Zaredzin <JammedMist76@hotmail.com>
2024-06-12 18:47:54 -0600

    Avance

--- app/index.jsx
+++ app/index.jsx
@@ -2,22 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
 import {Link} from 'expo-router';
 import tw from 'twrnc'
+import { TextInput } from 'react-native-web';
 
 
 export default function App() {
   return (
     <View style={tw ``}>
-      <View style={tw`flex bg-neutral-800 `}>
-        <Text style={tw`text-2xl text-white mt-10 mx-10`}>Pito guarzon 3 hola y adios</Text>
+      <View style={tw`flex mt-20 bg-neutral-800 pb-2 `}>
+        <Text style={ tw`font-pblack text-center text-4xl text-white mt-10 mx-10 `}>Iniciar sesión</Text>
         <StatusBar style="auto" />
       </View>     
-      <View style={tw `bg-neutral-300`}>
+      <View style={tw `bg-neutral-300 mx-10 rounded-md justify-center`}>
         <View>
+          <Text>Correo</Text>
           
         </View>
         <Link href="/profile" style={tw`flex text-cyan-500 mx-10 `}>Pito perfil</Link>
         
-      </View>    
+      </View>
+
       
       
       
